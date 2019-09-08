1b3d5675b5747575d6df65e4befb748b8e1745da
Omar Awamry <oawamry@rasan.co>
2019-09-09 00:09:26 +0200

    fix a comment

--- src/main.c
+++ src/main.c
@@ -5,22 +5,22 @@
 
 void main() {
     char data_in;
-    OSCCON = 0x72;              /* use internal oscillator frequency which is set to 8 MHz */
-    TRISA = 0x3E;               /* set PINA0 as output ports */
-    LATA  = 0x01;               /* set PINA0 high */
+    OSCCON  = 0x72;             /* use internal oscillator frequency which is set to 8 MHz */
+    TRISA   = 0x3E;             /* set PINA0 as output ports */
+    LATA    = 0x01;             /* set PINA0 high */
     USART_Init(9600);           /* initialize USART operation with 9600 baud rate */ 
     __delay_ms(50);
 
     while(1){
-        data_in=USART_ReceiveChar();
+        data_in = USART_ReceiveChar();
         if(data_in == '1') {
-            LATA0 = 1;                    /* turn ON LED */
-            USART_SendString("LED_ON\n"); /* send LED ON status to terminal */
+            LATA0 = 1;                      /* turn ON LED */
+            USART_SendString("LED_ON\n");   /* send LED ON status to terminal */
         } else if(data_in == '2') {
-            LATA0 = 0;                   /* turn OFF LED */
-            USART_SendString("LED_OFF\n");/* send LED ON status to terminal */
+            LATA0 = 0;                      /* turn OFF LED */
+            USART_SendString("LED_OFF\n");  /* send LED ON status to terminal */
         } else {
-            USART_SendString("extra");/* send msg to select proper option */
+            USART_SendString("extra");      /* send msg to select proper option */
         }
         __delay_ms(100);
     }
